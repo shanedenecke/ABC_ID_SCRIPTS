e1fb23b435b7fda776b044d423c8b6937f53425c
shanedenecke <shanedenecke@gmail.com>
2020-04-14 20:24:13 +0300

    nothing

--- ABC_temp.sh
+++ ABC_temp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-H=~/Transporter_ID/ABC_id
+H=/mnt/disk/shane/Transporter_ID/ABC_id
 PHYLO=$H/ABC_REF/Input_files/Phylo_list.txt
 SPEC=$H/ABC_REF/Input_files/target_species.tsv
 QUAL_THRESH=.3
@@ -7,15 +7,4 @@ THREADS=14
 
 cd $H 
 
-source ./ABC_ID_SCRIPTS/ABC_BUSCO.sh
-
-
-
-###### 3) Search proteomes
-mkdir ABC_search
-mkdir preliminary_ABC
-mkdir preliminary_ABC/proteomes
-mkdir preliminary_ABC/dicts
-for i in ./proteomes/*; do
-  source ./ABC_ID_SCRIPTS/ABC_search_join.sh $i
-done
+source ./ABC_ID_SCRIPTS/ABC_phylo.sh
