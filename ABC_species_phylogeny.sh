da64d00362b475f1a90b1676847c4cdd7fe524a8
shanedenecke <shanedenecke@gmail.com>
2020-04-22 10:48:40 +0300

    fixed problem with substituting out bad characters like J and . from fasta file. Just did it beforehand

--- ABC_species_phylogeny.sh
+++ ABC_species_phylogeny.sh
@@ -1,11 +1,12 @@
 cd $H 
 cd CAFE
 
-~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Hemi_new.txt -ortho_algo Orthofinder -outgroups "DroMel BomMor ApiMel AedAeg " -threads $THREADS
+~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Hemiptera_species_taxid.txt -ortho_algo Orthofinder -outgroups "DroMel BomMor ApiMel AedAeg " -threads $THREADS
 
-~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Lep_new.txt -ortho_algo Orthofinder -outgroups "MyzPer" -threads $THREADS
+~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Lepidoptera_species_taxid.txt -ortho_algo Orthofinder -outgroups "MyzPer" -threads $THREADS
 
-~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Dipt_new.txt -ortho_algo OrthoDB -outgroups "None" -threads $THREADS
+~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Diptera_species_taxid.txt -ortho_algo OrthoDB -outgroups "BomMor" -threads $THREADS
 
-~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Arth_new3.txt -ortho_algo Orthofinder -outgroups "CaeEle" -threads $THREADS
+~/Applications/Custom_Applications/Species_phylogeny.sh -taxid_codes ../ABC_REF/CAFE/Arthropod_species_taxid.txt -ortho_algo Orthofinder -outgroups "CaeEle" -threads $THREADS
 
+cp ./*/rax_output/RAxML_bipartitions.*.nwk ./clean_raxml_trees/
