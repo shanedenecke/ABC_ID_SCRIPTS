ade0f85aaf3b185f7b57b82784f794615efa43f7
shanedenecke <shanedenecke@gmail.com>
2020-04-22 10:48:52 +0300

    updates

--- ABC_ID_Wrapper.sh
+++ ABC_ID_Wrapper.sh
@@ -3,13 +3,13 @@ H=/mnt/disk/shane/Transporter_ID/ABC_id
 PHYLO=$H/ABC_REF/Input_files/Phylo_list.txt
 SPEC=$H/ABC_REF/Input_files/target_species.tsv
 QUAL_THRESH=.3
-THREADS=14
+THREADS=10
 
 cd $H 
 
 ###### 1) proteome prepare
-#source ./ABC_ID_SCRIPTS/ABC_proteome_prepare.sh
-source ./ABC_ID_SCRIPTS/ABC_proteome_add.sh
+source ./ABC_ID_SCRIPTS/ABC_proteome_prepare.sh
+#source ./ABC_ID_SCRIPTS/ABC_proteome_add.sh $H/ABC_REF/non_model_proteomes/append/*
 
 ###### 2) Build database materials
 source ./ABC_ID_SCRIPTS/ABC_Model_database_build_join.sh
@@ -17,9 +17,6 @@ source ./ABC_ID_SCRIPTS/ABC_Model_database_build_join.sh
 
 ##### 2.5) Run BUSCO for quality assessment
 source ./ABC_ID_SCRIPTS/ABC_BUSCO.sh
-
-
-
 
 ###### 3) Search proteomes
 mkdir ABC_search
@@ -36,8 +33,10 @@ Rscript ./ABC_ID_SCRIPTS/ABC_domain_filter_new.R
 ###### 5) CAFE
 mkdir CAFE
 mkdir ./CAFE/clean_raxml_trees
+
 #source ./ABC_ID_SCRIPTS/ABC_species_phylogeny.sh
-cp ./ABC_REF/ultrametric_tree_backup/*.tre ./CAFE/clean_raxml_trees/
+cp ./ABC_REF/ultrametric_tree_backup/*.nwk ./CAFE/clean_raxml_trees/
+
 Rscript ./ABC_ID_SCRIPTS/ABC_CAFE_prep.R  
 source ./ABC_ID_SCRIPTS/ABC_CAFE_run_full.sh
 Rscript ./ABC_ID_SCRIPTS/ABC_CAFE_figures.R
